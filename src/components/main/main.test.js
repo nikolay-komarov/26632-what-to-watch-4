762d62dab0318e36cec1d39bc463b3c32166c950
kny45 <kny.komarov@gmail.com>
2020-06-13 16:33:22 +0300

    Добавит snapshot-тесты для App, Main

---
+++ src/components/main/main.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Main from "./main.jsx";
+
+describe(`Render Main`, () => {
+  it(`Should Main render correctly`, () => {
+    const movieCard = {
+      title: `Star Wars`,
+      genre: `fantastic`,
+      year: `1977`
+    };
+    const movieList = [
+      `The Witcher`,
+      `The Expance`,
+      `Stranger Things`,
+      `Firefly`
+    ];
+
+    const tree = renderer
+      .create(
+          <Main
+            movieCard = {movieCard}
+            movieList = {movieList}
+          />)
+        .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
