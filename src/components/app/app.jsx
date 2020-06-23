2fb4df79386d1f3e11f9a23125e0d5239539b091
kny45 <kny.komarov@gmail.com>
2020-06-23 21:43:00 +0300

    Добавит router в App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 
 import Main from "../main/main.jsx";
+import MoviePage from "../movie-page/movie-page.jsx";
 
 const App = (props) => {
   const {
@@ -12,11 +14,21 @@ const App = (props) => {
   const handleSmallMovieCardClick = () => {};
 
   return (
-    <Main
-      movieCard = {movieCard}
-      moviesList = {moviesList}
-      onSmallMovieCardClick = {handleSmallMovieCardClick}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main
+            movieCard = {movieCard}
+            moviesList = {moviesList}
+            onSmallMovieCardClick = {handleSmallMovieCardClick}
+          />
+        </Route>
+        <Route exact path="/dev-film">
+          <MoviePage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
   );
 };
 
